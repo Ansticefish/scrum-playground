56764b706f6e257d3e7a909ad33c42d4da911139
Ansticefish <ansticefish0106@gmail.com>
2022-11-30 17:49:32 +0800

    Finish first part of Quest4

---
+++ src/components/SprintLesson.vue
@@ -0,0 +1,103 @@
+<template lang="pug">
+div.lessons-container
+  div.lesson(v-for="item in list" :key="item.id")
+    img(:src="item.img")
+    div.title-wrapper
+      h2 {{ item.title}}
+      p {{ item.subtitle}}
+    ul.lesson 
+      p {{ item.contentStart}}
+      li(v-if="item.contentList[0]") {{item.contentList[0]}}
+      li(v-if="item.contentList[1]") {{item.contentList[1]}}
+      li(v-if="item.contentList[2]") {{item.contentList[2]}}
+      p(v-if="item.contentEnd") {{ item.contentEnd}}
+</template>
+
+<script>
+export default {
+  name: 'SprintLesson',
+  data () {
+    return {
+      list: [
+        {
+          id: 1,
+          img: require('@/assets/image/quest4-daily.png'),
+          title: '每日站立會議',
+          subtitle: 'Daily Scrum',
+          contentStart: '每天都要進行的會議 ， 以15分鐘為限制',
+          contentList: [
+            '昨天為團隊的短衝目標 (Sprint Goal)做了那些進度',
+            '今天我會如何準備來幫助團隊達到短衝目標',
+            '過程中有遇到什麼問題、難題'
+          ],
+          contentEnd: '透過團隊分享 ， 追蹤大家的工作狀況。'
+        },
+        {
+          id: 2,
+          img: require('@/assets/image/quest4-review.png'),
+          title: '短衝檢視會議',
+          subtitle: 'Sprint Review',
+          contentStart: '用來檢視該次短衝增量的成果 ， 以蒐集相關的回饋數據或意見。',
+          contentList: [],
+          contentEnd: ''
+        },
+        {
+          id: 3,
+          img: require('@/assets/image/quest4-retro.png'),
+          title: '短衝自省會議',
+          subtitle: 'Sprint Retrospective',
+          contentStart: '團隊在自省會議裡,會共同回顧該短衝歷程發生的事情',
+          contentList: [
+            '好的地方',
+            '可以改進的地方',
+            '如何維持我們已有的成功經驗'
+          ],
+          contentEnd: ''
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.lessons-container {
+  width: 95%;
+  height: 90%;
+  margin: 2% auto;
+  padding: 2%;
+  @include flex (space-between, flex-start);
+  .lesson {
+    width: calc(100% /3 - 2%);
+    text-align: center;
+    img {
+      width: 100%;
+    }
+    .title-wrapper {
+      @include containerStyle(100%, 30%, $bg-dark60, 4px solid $role-team1, 20px);
+      margin: 5% auto;
+      padding: 8px 16px;
+      backdrop-filter: blur(5px);
+      h2 {
+        @extend %h2;
+        color: $role-team1;
+      }
+      p {
+        @extend %listCaption;
+        color: $text-tint;
+      }
+    }
+    .lesson {
+      width: 100%;
+      all: revert;
+      text-align: left;
+      list-style-position: inside;
+      p, li {
+        @extend %body;
+        line-height: 36px;
+        color: $text-default;
+      }    
+    }
+  }
+}
+</style>
--- src/views/QuestFour.vue
+++ src/views/QuestFour.vue
@@ -1,10 +1,74 @@
 <template lang="pug">
-div Quest-4
+div.quest4
+  div.head
+    //- dialogues top
+    Dialogue(
+      v-if="step === 1"
+      :speaker="'EE'"
+      :show-pointer="true"
+      :yellow="true"
+      @change-step="changeStep" 
+      )
+      | 等等等等等 ， 你都還不知道什麼是 Sprint 吧 ！
+      br
+      | 讓我先為你介紹一下～仔細聽好唷 ， 等等會考考你 ！
+    Dialogue(
+      v-if="step === 2"
+      :speaker="'EE'"
+      :show-pointer="true"
+      :yellow="true"
+      @change-step="changeStep" 
+      )
+      |  Sprint 是一個短衝 ， 開發團隊會在這期間執行開發 。 在這段期間內 ， 開發團隊舉辦
+      span 每日站立會議 (Daily Scrum) 
+      | ， 追蹤成員間的工作狀況 ， 在 Sprint 的結束也會包含
+      span 短衝檢視會議 (Sprint Review)
+      | 以及
+      span 短衝自省會議 (Sprint Retrospective) 
+      |。
+    Dialogue(
+      v-if="step === 3"
+      :speaker="'EE'"
+      :yellow="true"
+      )
+      | 優化工作流程、讓團隊有變得更好的機會。
+      br
+      | 推薦工具 ： 
+      img(src="~@/assets/image/logo_confluence.png")
+    Character.character(
+      :color="'yellow'"
+      )
+  div.body
+    SprintLesson(v-if="step === 2 || step === 3")
+    button.btn-next(
+      v-if="step === 3" 
+      @click="changeStep"
+      ) 練習去囉
+      div 練習去囉
 </template>
 
 <script>
+import Dialogue from '../components/Dialogue.vue';
+import Character from '../components/Character.vue';
+import SprintLesson from '../components/SprintLesson.vue';
+
 export default {
   name: 'QuestFour',
+  components: {
+    Dialogue,
+    Character,
+    SprintLesson,
+  },
+  data () {
+    return {
+      step: 1,
+    }
+  },
+  methods: {
+    changeStep () {
+      this.step ++
+    }
+  },
   beforeCreate() {
     this.$store.commit('updateProgress', '66.6vw')
   }
@@ -12,5 +76,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.quest4 {
+  @include containerStyle(100vw, 100vh, url('~@/assets/image/bg.png'));
+  padding: 2% 0;
 
+  .head {
+    @include flex (space-around, center);
+    width: 100%;
+    height: 30%;
+
+    .character {
+      @extend %character;
+      height: 130%;
+      margin-top: 5%;
+    }
+  }
+
+  .body {
+    height: 70%;
+    position: relative;
+    .btn-next {
+      @include button (10vw, 60px, $button-linear, $primary-default, $text-default);
+      @include position (absolute, $bottom: 8%, $right: 2%);
+    }
+  }
+}
 </style>
